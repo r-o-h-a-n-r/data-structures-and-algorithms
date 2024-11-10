7dc2b9c4813a968c3d479a9302a7a436c6e03779
Rohan R <meet.rohan.ravi@gmail.com>
2024-11-10 18:42:03 +0530

    Initial Commit - LinkedList

--- DataStructuresAndAlgorithms.java
+++ DataStructuresAndAlgorithms.java
@@ -1,5 +1,8 @@
+import linkedlist.LinkedList;
+
 public class DataStructuresAndAlgorithms {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        LinkedList linkedList = new LinkedList();
     }
 }
